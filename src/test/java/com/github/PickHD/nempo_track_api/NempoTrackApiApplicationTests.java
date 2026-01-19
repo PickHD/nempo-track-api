65edbe70dbe34865a970a63244e5167c3c19870b
Taufik Januar <taufikjanuar35@gmail.com>
2026-01-19 11:40:08 +0700

    chore: initialize project & dependencies pom.xml

---
+++ src/test/java/com/github/PickHD/nempo_track_api/NempoTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.PickHD.nempo_track_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NempoTrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
